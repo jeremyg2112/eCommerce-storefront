a9e7b8dae1d75ad72f2843b18d48edbef0180a4c
Jeremy Galloway <jeremyg2112@gmail.com>
2019-04-28 02:36:43 -0400

    added cart functionality (incomplete) and ability to change quantity of items in cart

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+
+// DOM SELECTORS
 const boxes = document.getElementsByClassName("product-box");
 const galleryElemOne = document.getElementById("sale");
 const galleryElemTwo = document.getElementById("products-container");
 const productDetailOne = document.getElementById("product-detail-view");
 const productDetailTwo = document.getElementById("product-detail-desc");
+const emptyCart = document.getElementById("my-cart-empty");
 const returnToGallery = document.getElementById("fake-link");
+const cart = document.getElementById("cart-contents");
+const addToCartButton = document.getElementById("add-cart-btn");
+const cartTotal = document.getElementById("cart-total");
+
+const dataId = document.createAttribute("dataId");
+
+// stores contents of cart as array of objects
+let myCart = [];
 
 
 function createHTML(arr) {
@@ -14,18 +25,19 @@ function createHTML(arr) {
 
  	// will listen for products in gallery being clicked 
  		boxes[i].addEventListener("click", function(e) {
- 			
- 	// finds data corresponding to clicked product and prepares HTML 	
+ 		// finds data corresponding to clicked product and prepares HTML for product details	
  			data.forEach(function(obj) {		
  				if (obj.productId == boxes[i].id) {
  					document.getElementById("product-detail-title").innerText = obj.productName;
  					document.getElementById("product-price").innerText = '$' + obj.price;
  					document.getElementById("product-description").innerText = obj.desc;
  					document.getElementById("product-detail-main-photo").src = obj.productImgSrc;
+ 					
+ 					dataId.value = obj.productId;
+ 					addToCartButton.setAttributeNode(dataId);
  				}
  			})
-
- 	// clears product gallery elements, changes page to "product detail" view
+ 		// clears product gallery elements, changes page to "product detail" view
  			galleryElemOne.style = "display: none;";
  			galleryElemTwo.style = "display: none;";
  			productDetailOne.style = "display: grid;";
@@ -73,4 +85,135 @@ returnToGallery.addEventListener("click", function(){
  	
 })
 
+class CartItem {
+  constructor(productId, price, quantity) {
+    this.productId = productId;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
+
+
+
+function calculateTotal() {
+	let total = 0;
+	myCart.forEach(function(obj) {
+		return total += obj.price * obj.quantity;
+	})
+	cartTotal.innerHTML = "$" + total;
+}			
+
+function findIndex(arr, val) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].productId == val) {
+			return i;
+		} 
+	}
+	return -1;	
+}
+
+function checkIfCartItems() {
+	let items = document.getElementsByClassName("cart-item");
+	if (items.length < 1) {
+		cart.style = "display: none;";
+		emptyCart.style = "display: flex;";
+		console.log("cart has been emptied");
+	} else {
+		console.log("still items in the cart");
+	}
+}
+
+function increaseQuantity(id) {
+		let counter = document.getElementById(`${id}_counter`);
+		let index = findIndex(myCart, id);
+		myCart[index].quantity++;
+		counter.innerText = myCart[index].quantity;
+
+
+	// let counter = document.getElementById(`${id}_counter`);
+	// myCart.forEach(function(obj) {
+	// 	if (obj.productId == id) {
+	// 		obj.quantity++;
+	// 		counter.innerText = obj.quantity;
+	// 	}
+	// })
+}
+
+
+function decreaseQuantity(id) {
+		let counter = document.getElementById(`${id}_counter`);
+		let index = findIndex(myCart, id);
+		myCart[index].quantity--;
+		counter.innerText = myCart[index].quantity;
+		if (myCart[index].quantity < 1) {
+			let itemToDelete =  document.getElementById(`${id}_cart`);
+			itemToDelete.parentNode.removeChild(itemToDelete);
+			checkIfCartItems();
+		}
+	}
+
+
+	// let counter = document.getElementById(`${id}_counter`);
+	// myCart.forEach(function(obj) {
+	// 	if (obj.productId == id) {
+	// 		obj.quantity--;
+	// 		counter.innerText = obj.quantity;
+	// 		console.log(obj);
+	// 	}
+	// })
+
+
+
+addToCartButton.addEventListener("click", function(e) {
+
+	let dataIdHolder = dataId.value;
+	
+	if (!document.getElementById(`${dataIdHolder}_cart`)) {
+		// creates new cart item if one does not already exist for selected product
+	data.forEach(function(obj) {		
+ 				if (obj.productId == dataIdHolder) {
+ 		
+ 					let newDiv = document.createElement("div");
+ 					newDiv.id = `${dataIdHolder}_cart`;
+ 					newDiv.className = "cart-item";
+
+ 					newDiv.innerHTML= 
+						`<div class="cart-thumb">
+							<img class="cart-thumb-img" src="${obj.productImgSrc}">
+						</div>
+						<div class="cart-item-info">
+							<p>${obj.productName}</p>
+							<p>$${obj.price}</p>
+						</div>
+						<div class="cart-quantity">
+							<button>
+								<div class="quantity-btn" id="${dataIdHolder}_subtract">-</div>
+								<span id="${dataIdHolder}_counter">1</span>
+								<div class="quantity-btn" id="${dataIdHolder}_add">+</div>
+							</button>
+						</div>`;
+					emptyCart.style = "display: none;";
+					cart.style = "display: block;";
+					cart.appendChild(newDiv);
+
+					let newCartItem = new CartItem(dataIdHolder, obj.price, 1);
+					myCart.push(newCartItem);
+					
+					document.getElementById(`${dataIdHolder}_add`).addEventListener("click", function() {
+						increaseQuantity(dataIdHolder);
+						calculateTotal();
+					}); 
+					document.getElementById(`${dataIdHolder}_subtract`).addEventListener("click", function() {
+						decreaseQuantity(dataIdHolder);
+						calculateTotal();
+					}); 
+					calculateTotal();
+ 				}
+ 			})
+	} else {
+		// increases quantity if product already exists in cart  
+		increaseQuantity(dataIdHolder);
+		calculateTotal();
+	}
+})
 
